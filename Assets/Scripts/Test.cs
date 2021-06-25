f22b5e242e24ec88d99c56514eabe5b818c04b04
Aldhair Vera Camacho <u201315853@upc.edu.pe>
2021-06-25 14:19:46 -0500

    Learning about conditionals

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Directions
+{
+    North, South, West, East
+}
+
+public class Test : MonoBehaviour
+{
+    public string name;
+    public Directions directions;
+
+    void Start()
+    {
+        //<, >, ==, <=, >=
+        //&& -> AND -> true && true -> true / false && true -> false
+        //|| -> Or -> true || true -> true / false || true -> true / false || false -> false
+        //!= -> Diferent 
+
+        switch (directions)
+        {
+            case Directions.North:
+                print("Your direction is north");
+                break;
+            case Directions.South:
+            case Directions.East:
+                print("Your direction is south or east");
+                break;
+            default:
+                print("Your direction is west");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
